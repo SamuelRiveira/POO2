5374d30f81a1400b6de1ddaa0c33d405acad24f3
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-03-11 12:46:04 +0000

    clase transporte

---
+++ transporte.py
@@ -0,0 +1,10 @@
+class Transporte:
+    def __init__(self, ruedas, asientos):
+        self.ruedas = ruedas
+        self.asientos = asientos
+
+    def desplazar(self, x, y):
+        print(f"Moviendose a x: {x} metros y: {y} metros")
+
+    def informacion(self):
+        print("Guaguas, tu aliado ágil y accesible para explorar ciudades y conectar comunidades. Comodidad, eficiencia y ecología en cada viaje. Descubre la libertad de movilidad sin complicaciones.")
