79e2169e6e921f35080e80c26bf6ef0dca71a9a9
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-03-11 13:01:35 +0000

    Cambio información

--- mainHerencia.py
+++ mainHerencia.py
@@ -2,11 +2,11 @@ from guagua import Guagua
 
 def main():
     intercity = Guagua(6, 50)
-    print(f"ruedas de intercity: {intercity.ruedas} asientos de intercity: {intercity.asientos}")
+    intercity.informacion()
     intercity.desplazar(10, 24)
 
     lanzaroteBus = Guagua(7, 30)
-    print(f"ruedas de intercity: {lanzaroteBus.ruedas} asientos de intercity: {lanzaroteBus.asientos}")
+    lanzaroteBus.informacion()
     lanzaroteBus.desplazar(20, 5)
 
 if __name__ == "__main__":
