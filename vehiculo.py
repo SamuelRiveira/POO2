a80c9ed546f08ca7723effe04cb26b494ffe8e36
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-03-12 11:49:56 +0000

    Separación

--- vehiculo.py
+++ vehiculo.py
@@ -1,7 +1,9 @@
 class Vehiculo:
+    
     def __init__(self, ruedas:int, color:str) -> None:
         self.ruedas = ruedas
         self.color = color
+    
     def info(self):
         print(f"Ruedas: {self.ruedas} Color: {self.color}")
     
